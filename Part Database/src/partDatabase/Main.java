4ea2ba894871dad37a2c7725bc6c49f282cade80
Jesse King <jessifin@gmail.com>
2014-02-01 12:08:01 -0500

    Yay, code.

--- Part Database/src/partDatabase/Main.java
+++ Part Database/src/partDatabase/Main.java
@@ -1,5 +1,25 @@
 package partDatabase;
 
-public class Main {
+import javax.swing.JFrame;
 
+public class Main {
+	
+	private static JFrame frame;
+	
+	private static void init() {
+		frame = new JFrame();
+		frame.setTitle("Part Database");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	private static void destroy() {
+		System.exit(0);
+	}
+
+	public static void main(String... args) {
+		init();
+		
+		destroy();
+	}
 }
