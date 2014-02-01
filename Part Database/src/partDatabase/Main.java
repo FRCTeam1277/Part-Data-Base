6517827e10dfb34075bf79f5493ee64f6e0234bf
Jesse King <jessifin@gmail.com>
2014-02-01 12:35:10 -0500

    Derp, derp.

--- Part Database/src/partDatabase/Main.java
+++ Part Database/src/partDatabase/Main.java
@@ -1,15 +1,30 @@
 package partDatabase;
 
+import java.awt.DisplayMode;
+import java.awt.GraphicsEnvironment;
+
 import javax.swing.JFrame;
 
 public class Main {
 	
 	private static JFrame frame;
+	private static final int SCREEN_WIDTH, SCREEN_HEIGHT;
+	public static boolean running = true;
+	
+	static {
+		DisplayMode dm = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode();
+
+		SCREEN_WIDTH = dm.getWidth();
+		SCREEN_HEIGHT = dm.getHeight();
+	}
 	
 	private static void init() {
 		frame = new JFrame();
 		frame.setTitle("Part Database");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.setBounds(SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+		
 		frame.setVisible(true);
 	}
 	
@@ -19,6 +34,10 @@ public class Main {
 
 	public static void main(String... args) {
 		init();
+		
+		while(running) {
+			
+		}
 		
 		destroy();
 	}
